2dd8bd51260547a1b882a81797f4346611033e50
jack <vip9renxing@hotmail.com>
2020-07-10 09:53:09 +0800

    提交Demo

---
+++ test1/src/main/java/com/zlk/Demo.java
@@ -0,0 +1,14 @@
+package com.zlk;
+
+/**
+ * @program: workspace-changchun
+ * @description:
+ * @author: lth
+ * @create: 2020-07-10 09:47
+ **/
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("测试提交内容");
+    }
+}
