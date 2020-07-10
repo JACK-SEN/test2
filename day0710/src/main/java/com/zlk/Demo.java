84770288f328ae2f20fb57798eea7c8a009479a7
jack <vip9renxing@hotmail.com>
2020-07-10 10:52:42 +0800

    测试提交

---
+++ day0710/src/main/java/com/zlk/Demo.java
@@ -0,0 +1,18 @@
+package com.zlk;
+
+/**
+ * @program: workspace-changchun
+ * @description:
+ * @author: lth
+ * @create: 2020-07-10 10:50
+ **/
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("测试测试");
+        System.out.println("测试测试");
+        System.out.println("测试测试");
+        System.out.println("测试测试");
+        System.out.println("测试测试");
+    }
+}
